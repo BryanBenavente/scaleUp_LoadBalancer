1ebb0c88c052cc7e7fa9588e228368e1869ae74c
Bryan.Benavente <bryan.benavente@vallegrande.edu.pe>
2020-12-08 20:16:02 -0500

    Levantando Zuul

---
+++ zuul/src/main/java/com/microservicios/zuul/ZuulApplication.java
@@ -0,0 +1,23 @@
+package com.microservicios.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
